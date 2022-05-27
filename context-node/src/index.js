a7f6493d437b94212b698575c760512678b75e42
Chris Okerberg <chris@disastertech.com>
2022-05-27 12:54:05 -0400

    Working commit: trouble importing json

--- context-node/src/index.js
+++ context-node/src/index.js
@@ -3,6 +3,9 @@
 import express from 'express'
 import ash from 'express-async-handler'
 
+import Clients from './clients.json' assert {type: 'json'}
+// import { readFile } from 'fs';
+
 export const app = express();
 
 app.use(express.json());
@@ -20,3 +23,18 @@ app.get('/healthcheck', ash(async(req, res) => {
   const status = {"status":"OK"};
   res.send(status);
 }));
+
+
+app.get('/client_list', ash(async(req, res) => {
+
+  res.send(Clients)
+  // typically, such things would come from a database in a real implementation. 
+  // readFile('./clients.json', (err, source) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     const json = JSON.parse(source)
+  //     res.send(json)
+  //   }
+  // });
+}))
