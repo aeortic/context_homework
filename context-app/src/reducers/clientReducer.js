b4a0cc26b32b9372860a20b95bca0f061ea22d91
Chris Okerberg <chris@disastertech.com>
2022-05-29 11:28:06 -0400

    Fixed bug caused by incorrect action contant

--- context-app/src/reducers/clientReducer.js
+++ context-app/src/reducers/clientReducer.js
@@ -1,5 +1,5 @@
 import {
-  LOAD_CLIENT_LIST_REQUEST,
+  LOAD_CLIENT_LIST_SUCCESS,
 } from '../constants/client-action-types';
 
 const initialState = {
@@ -9,7 +9,7 @@ const initialState = {
 const clientReducer = (state = initialState, action) => {
 
   switch (action.type) {
-    case  LOAD_CLIENT_LIST_REQUEST:
+    case  LOAD_CLIENT_LIST_SUCCESS:
       return {
         ...state,
         clientList: action.payload,
