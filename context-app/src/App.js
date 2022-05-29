9692df20af146d1dd3efaefaf6530dd8550b5be0
Chris Okerberg <chris@disastertech.com>
2022-05-29 13:51:16 -0400

    A bit of merge cleanup

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -1,3 +1,5 @@
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import Title from './components/DesignSystems/Text/Title'
 import ClientList from './components/ClientList'
 import './App.css';
@@ -17,30 +19,6 @@ export default function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <header className="App-header">
-        Client List
-      </header>
-      <ul>{clientList.map(client => (
-        <li key={`client-${client.name}`}>
-          {
-            JSON.stringify(client, null, 2)
-          }
-        </li>
-      ))}</ul>
       <div className="App-header">
         <Title>
           Client List
